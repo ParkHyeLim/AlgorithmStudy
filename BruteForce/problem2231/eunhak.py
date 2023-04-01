35dd7318841437cfa55f4e5a98b670e4f3e4cda4
남연진 <ygeenee@naver.com>
2023-04-01 12:21:46 +0900

    greedy

--- BruteForce/problem2231/eunhak.py
+++ BruteForce/problem2231/eunhak.py
@@ -1,4 +1,4 @@
-## 방법 1 시간초과
+# 방법 1 시간초과
 # N = int(input())
 # m = 1       # 자연수 1부터 전체탐색
 
@@ -6,7 +6,7 @@
 #     sum = 0          # 각 자리 수 합
 #     for i in range( len(str(m)) ):
 #         sum += int( str(m)[i] )
-    
+
 #     if (sum + m) == N:      # m의 분해합이 N인 경우
 #         M = m         # m을 생성자 M이라 함
 #         break
@@ -14,20 +14,19 @@
 # print(M)
 
 # # ----------방법2--------- 역시 시간초과
-# def sum_func(m):
-#         sum=m
-#         while (m!=0):
-#             sum+=m%10
-#             m=m//10
-#         return sum
+def sum_func(m):
+    sum = m
+    while (m != 0):
+        sum += m % 10
+        m = m//10
+    return sum
 
 
-# N = int(input())
+N = int(input())
 
-# M=0
-# while True:
-#     M+=1
-#     if sum_func(M)==N:
-#         break
-# print(M)
-
+M = 0
+while (M < N):
+    M += 1
+    if sum_func(M) == N:
+        break
+print(M)
--- BruteForce/problem2798/yeonjin.py
+++ BruteForce/problem2798/yeonjin.py
@@ -2,14 +2,14 @@
 n, m = map(int, input().split())
 arr = input().split()
 for e in range(len(arr)):
-  arr[e] = int(arr[e])
+    arr[e] = int(arr[e])
 
 answer = 0
 for i in range(n-2):
-  for j in range(i+1, n-1):
-    for k in range(j+1, n):
-      tmp = int(arr[i]+arr[j]+arr[k])
-      if (answer < tmp) and (tmp <= m):
-        answer = tmp
-        
-print(answer)     
+    for j in range(i+1, n-1):
+        for k in range(j+1, n):
+            tmp = int(arr[i]+arr[j]+arr[k])
+            if (answer < tmp) and (tmp <= m):
+                answer = tmp
+
+print(answer)
